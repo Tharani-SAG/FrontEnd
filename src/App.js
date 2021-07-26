04622374fd93699663919d673032e31d15aabc85
Tharani-SAG <85284468+Tharani-SAG@users.noreply.github.com>
2021-07-26 15:40:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import "./styles.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello There</h1>
+      </div>
+    );
+  }
+}
+export default App;
